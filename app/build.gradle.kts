d690dd78eca5393221a895249052a16d8600ca2f
HenleyR_cvsh <henleyr@aetna.com>
2026-04-24 10:43:50 -0400

    Updates to Android Studio Panda 4, AGP 9.2.0, Kotlin 2.3.21, and other library dependencies; targets Android API 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 }
 
 android {
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.cvshealth.accessibility.apps.androidviewaccessibilitytechniques"
         minSdk = 23
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
